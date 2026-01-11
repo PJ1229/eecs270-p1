57b8d2ba43bcae57b7921e017defb46d52159ece
PJ1229 <peterjoe2005@gmail.com>
2026-01-11 18:32:32 -0500

    init

---
+++ Selector.v
@@ -0,0 +1,10 @@
+// File Name: Selector.v
+// Implements a 2-to-1 7-bit MUX
+module Selector(
+	input sel,            // Selector control input
+	input [6:0] A, B,  // Selector data inputs
+	output [6:0] F		  // Selector data outputs
+	);
+	
+
+endmodule // Selector
