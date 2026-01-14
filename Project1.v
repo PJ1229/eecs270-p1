e3eb6b6660b2bede4ee7f794fb994aeb03cb486f
PJ1229 <peterjoe2005@gmail.com>
2026-01-14 13:24:57 -0500

    flipped the not and fixed 7seg

--- Project1.v
+++ Project1.v
@@ -9,11 +9,8 @@ module Project1(
 // instantiate selector module
 	wire [6:0] A = SW[6:0];
 	wire [16:10] B = SW[16:10];
-
-	wire [6:0] hexA = 7'b0001000;
-	wire [6:0] hexB = 7'b0000000;
 	
     Selector S1(KEY, A, B, LEDR);
-	Selector S2(KEY, hexA, hexB, HEX0);
+	Selector S2(KEY, A, B, HEX0);
 
 endmodule // Project1
