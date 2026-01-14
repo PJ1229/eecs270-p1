d18f51e8297f5eb1543ff56183065a6e22645f08
Kim <pekim@UMICH.EDU>
2026-01-14 13:52:04 -0500

    tested on tb

--- TestBench1.v
+++ TestBench1.v
@@ -2,45 +2,44 @@
 
 module tb;
 
-    // Testbench signals
-    reg  [3:0] SW;
-    reg        KEY;
+  reg  [17:0] SW;
+  reg  [3:3]  KEY;
+  wire [6:0]  LEDR;
+  wire [6:0]  HEX0;
 
-    integer i;
+  integer i;
 
-    // DUT instantiation (edit name/ports if needed)
-    // -----------------------------------------------
-    // example:
-    // my_module dut (
-    //     .SW(SW),
-    //     .KEY(KEY),
-    //     .OUT(OUT)
-    // );
-    // -----------------------------------------------
 
-    initial begin
-        // Initialize
-        SW  = 4'b0000;
-        KEY = 1'b0;
-        #5;
+  Project1 dut (
+    .KEY (KEY),
+    .SW  (SW),
+    .LEDR(LEDR),
+    .HEX0(HEX0)
+  );
 
-        // Test KEY = 0
-        KEY = 1'b0;
-        for (i = 0; i < 16; i = i + 1) begin
-            SW = i[3:0];
-            #5;
-        end
+  initial begin
+    SW  = 18'b0;
+    KEY = 1'b0;
+    #5;
 
-        // Test KEY = 1
-        KEY = 1'b1;
-        for (i = 0; i < 16; i = i + 1) begin
-            SW = i[3:0];
-            #5;
-        end
-
-        // End simulation
-        #10;
-        $stop;
+    KEY = 1'b0;
+    for (i = 0; i < 128; i = i + 1) begin
+      SW[6:0]   = i[6:0];
+      SW[17:7]  = 11'b0;
+      #5;
     end
 
+    KEY = 1'b1;
+    for (i = 0; i < 128; i = i + 1) begin
+      SW[16:10] = i[6:0];
+      SW[9:0]   = 10'b0;
+      SW[17]    = 1'b0;
+      #5;
+    end
+
+    #10;
+    $stop;
+  end
+
 endmodule
+
